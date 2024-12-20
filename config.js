c2e783ece5ba59b98a1fbd06c7158e272868f3b4
Salman Dabbakuti <29351207+Salmandabbakuti@users.noreply.github.com>
2024-12-20 13:01:53 +0530

    WIP: webhook handler, callbacks refactor

--- config.js
+++ config.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID;
 const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET;
+const RAZORPAY_WEBHOOK_SECRET = process.env.RAZORPAY_WEBHOOK_SECRET;
 
 module.exports = {
   RAZORPAY_KEY_ID,
   RAZORPAY_KEY_SECRET,
+  RAZORPAY_WEBHOOK_SECRET,
 };
